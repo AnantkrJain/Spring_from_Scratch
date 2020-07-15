32975666e97973483d7fa31b707769fcfcafb292
ANANT KUMAR JAIN <anantkr.developer@gmail.com>
2020-07-15 11:37:22 +0530

    Constructor and Setter Injection

--- Spring-Modules/01. Spring Core/spring-demo-one/src/com/udemy/springdemo/SetterDemoApp.java
+++ Spring-Modules/01. Spring Core/spring-demo-one/src/com/udemy/springdemo/SetterDemoApp.java
@@ -2,25 +2,24 @@ package com.udemy.springdemo;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class HelloSpringApp {
-
+public class SetterDemoApp {
 	public static void main(String[] args) {
 		
 		//load the spring configuration file
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		//retrieve bean from spring container
-		Coach theCoach1 = context.getBean("myCoach1", Coach.class);
-		Coach theCoach2 = context.getBean("myCoach2", Coach.class);
+		CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
 		
 		//call methods on bean
-		System.out.println(theCoach1.getDailyWorkout());
-		System.out.println(theCoach2.getDailyWorkout());
-		
-		System.out.println(theCoach1.getDailyFortune());
-		System.out.println(theCoach2.getDailyFortune());
+		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyFortune());
+		//call new methods to get literal values
+		System.out.println(theCoach.getEmailAddress());
+		System.out.println(theCoach.getTeam());
 		
 		//close the context
 		context.close();
 	}
 }
+;
