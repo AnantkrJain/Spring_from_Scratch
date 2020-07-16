aee5e6160135443b48c0a0b77779ac3b37cf3406
ANANT KUMAR JAIN <anantkr.developer@gmail.com>
2020-07-16 12:50:41 +0530

    Implementation for Bean Lifecycle, Constructor and Setter Injection of Mixed(Annotation and XML) Configuration

--- Spring-Modules/01. Spring Core/spring-demo-annotations/src/com/udemy/springdemo/AnnotationDemoApp.java
+++ Spring-Modules/01. Spring Core/spring-demo-annotations/src/com/udemy/springdemo/AnnotationDemoApp.java
@@ -9,10 +9,15 @@ public class AnnotationDemoApp {
 		ClassPathXmlApplicationContext context  = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		//get the bean from spring container
-		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		TennisCoach theCoach = (TennisCoach) context.getBean("tennisCoach", Coach.class);
 		
 		//call a method on the bean
 		System.out.println(theCoach.getDailyWorkout());
+		//call method to get the use of costructor/setter/field injection
+		System.out.println(theCoach.getDailyFortune());
+		//call out new methods to inject the properties values
+		System.out.println(theCoach.getEmail());
+		System.out.println(theCoach.getTeam());
 		
 		//close the context
 		context.close();
--- Spring-Modules/01. Spring Core/spring-demo-annotations/src/com/udemy/springdemo/TennisCoach.java
+++ Spring-Modules/01. Spring Core/spring-demo-annotations/src/com/udemy/springdemo/TennisCoach.java
@@ -1,14 +1,82 @@
 package com.udemy.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 //@Component("thatSillyCoach")
 @Component
+//@Scope("prototype")
 public class TennisCoach implements Coach {
+	
+	@Autowired
+	@Qualifier("randomFortuneService")
+	private FortuneService fortuneService;
+	
+	@Value("${foo.email}")
+	private String email;
+	
+	@Value("${foo.team}")
+	private String team;
+	
+	//Define a default constructor
+	public TennisCoach() {
+		System.out.println(">> TennisCoach:  inside default constructor");
+	}
+	
+	//define my init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println(">> TennisCoach:  inside doMyStartupStuff()");
+	}
+	
+	//define my destroy method
+	@PreDestroy
+		public void doMyCleanupStuff() {
+			System.out.println(">> TennisCoach:  inside doMyCleanupStuff()");
+		}
+		
+	/*
+	//Spring will use any method for dependency injection, setter method is not compulsory
+	@Autowired
+	public void doSomeStuff(FortuneService fortuneService) {
+		System.out.println(">> TennisCoach:  inside doSomeStuff() method");
+		this.fortuneService = fortuneService;
+	}
+	
+	//define a setter method
+	@Autowired
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println(">> TennisCoach:  inside setFortuneService() method");
+		this.fortuneService = fortuneService;
+	}
+
+	@Autowired
+	public TennisCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+	*/
 
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand vollley";
 	}
 
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getTeam() {
+		return team;
+	}
 }
