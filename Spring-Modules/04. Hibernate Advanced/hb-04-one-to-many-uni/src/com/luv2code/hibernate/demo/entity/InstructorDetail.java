f86ccab34ba5ae87aeb4af3704ddd78e2bf0fc09
ANANT KUMAR JAIN <anantkr.developer@gmail.com>
2020-07-23 11:28:51 +0530

    Implemented project for Hibernate @OneToMany mapping - unidirectional

---
+++ Spring-Modules/04. Hibernate Advanced/hb-04-one-to-many-uni/src/com/luv2code/hibernate/demo/entity/InstructorDetail.java
@@ -0,0 +1,88 @@
+package com.luv2code.hibernate.demo.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+//Annotate the class as an entity and map to db table
+@Entity
+@Table(name="instructor_detail")
+public class InstructorDetail {
+	
+	//Define the fields
+	//Annotate the fields with db column names
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="youtube_channel")
+	private String youtubeChannel;
+	
+	@Column(name="hobby")
+	private String hobby;
+	
+	//Add new field for Instructor (Also get getters/setters)
+	//@OneToOne(mappedBy="instructorDetail", cascade=CascadeType.ALL)                  
+	
+	//Delete only instructor detail but keep the instructor ---> Since we are not using REMOVE CascadeType
+	@OneToOne(mappedBy="instructorDetail", cascade= {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+	private Instructor instructor;
+	
+	public Instructor getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
+	}
+
+	
+	//Create constructors
+	public InstructorDetail() {
+		
+	}
+
+	public InstructorDetail(String youtubeChannel, String hobby) {
+		super();
+		this.youtubeChannel = youtubeChannel;
+		this.hobby = hobby;
+	}
+
+		
+	//generate getters/setters methods
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getYoutubeChannel() {
+		return youtubeChannel;
+	}
+
+	public void setYoutubeChannel(String youtubeChannel) {
+		this.youtubeChannel = youtubeChannel;
+	}
+
+	public String getHobby() {
+		return hobby;
+	}
+
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+
+	//generate toString() method
+	@Override
+	public String toString() {
+		return "InstructorDetail [id=" + id + ", youtubeChannel=" + youtubeChannel + ", hobby=" + hobby + "]";
+	}
+}
