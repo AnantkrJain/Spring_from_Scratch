adf3ed22f51e52d224057633351019807cadcd0b
ANANT KUMAR JAIN <anantkr.developer@gmail.com>
2020-07-29 08:22:29 +0530

    Implemented @Around advice type

---
+++ Spring-Modules/06. Aspect-Oriented Programming/09. spring-demo-aop-around/src/com/luv2code/aopdemo/dao/MembershipDAO.java
@@ -0,0 +1,16 @@
+package com.luv2code.aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MembershipDAO {
+	
+	public boolean addMember() {
+		System.out.println(getClass()+" : DOING STUFF: ADDING A MEMBERSHIP ACCOUNT\n");	
+		return true;
+	}
+	
+	public void goToSleep() {
+		System.out.println(getClass() + " : I am going to sleep now . . .\n");
+	}
+}
